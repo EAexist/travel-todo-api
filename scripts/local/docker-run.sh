78312930f7cd30ba53366c71f8f1b883bed56ce9
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-11-18 23:24:58 +0900

    ci: local docker run script

--- scripts/local/docker-run.sh
+++ scripts/local/docker-run.sh
@@ -3,14 +3,14 @@
 DB_COMPOSE_FILE="docker-compose-db.yml"
 APP_COMPOSE_FILE="docker-compose.yml"
 
-./gradlew bootBuildImage -x test && \
+(
+    # --- 1. Clean up old conflicting containers first ---
+    echo "Stopping existing containers..."
+    docker compose -f "$DB_COMPOSE_FILE" -f "$APP_COMPOSE_FILE" stop
 
-# --- 1. Clean up old conflicting containers first ---
-echo "Stopping existing containers..."
-docker compose -f "$DB_COMPOSE_FILE" -f "$APP_COMPOSE_FILE" stop
+    # --- 2. Build and Start all services in a single project ---
+    echo "Starting services from $DB_COMPOSE_FILE and $APP_COMPOSE_FILE..."
+    docker compose -f "$DB_COMPOSE_FILE" -f "$APP_COMPOSE_FILE" up -d
 
-# --- 2. Build and Start all services in a single project ---
-echo "Starting services from $DB_COMPOSE_FILE and $APP_COMPOSE_FILE..."
-docker compose -f "$DB_COMPOSE_FILE" -f "$APP_COMPOSE_FILE" up -d
-
-echo "Done! Services are running."
+    echo "Done! Services are running."
+)
