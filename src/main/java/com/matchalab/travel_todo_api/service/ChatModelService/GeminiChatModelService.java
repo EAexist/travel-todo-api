141a7ec9a110894f092ae1888adf2a08b58f187c
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-01-23 20:45:18 +0900

    refactor: run with env variables in dev / prod profile

--- src/main/java/com/matchalab/travel_todo_api/service/ChatModelService/GeminiChatModelService.java
+++ src/main/java/com/matchalab/travel_todo_api/service/ChatModelService/GeminiChatModelService.java
@@ -21,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Profile({ "!development" })
+@Profile({ "prod" })
 public class GeminiChatModelService implements ChatModelService {
 
     // private final VertexAiGeminiChatModel chatModel = new
--- src/main/java/com/matchalab/travel_todo_api/service/ChatModelService/MockChatModelService.java
+++ src/main/java/com/matchalab/travel_todo_api/service/ChatModelService/MockChatModelService.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Profile({ "development" })
+@Profile({ "!prod" })
 public class MockChatModelService implements ChatModelService {
 
     @Autowired
